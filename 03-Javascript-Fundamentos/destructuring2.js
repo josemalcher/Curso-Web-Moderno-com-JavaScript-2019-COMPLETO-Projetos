854a780a24de5e62997cc621d1e6054794150aa8
josemalcher <contato@josemalcher.net>
2020-01-02 11:10:30 -0300

    ajustes

--- 03-Javascript-Fundamentos/destructuring2.js
+++ 03-Javascript-Fundamentos/destructuring2.js
@@ -1,8 +1,12 @@
 const [a] = [10]
 console.log(a)
 
-const [n1,n2, n3, n4 = 22] = [10,30,9,9]
-console.log(n1,n2,n3,n4)
+//const [n1,n2, n3, n4 = 22] = [10,30,9,9]
+//console.log(n1,n2,n3,n4)
+
+const [n1, , n3, , n5, n6 = 22] = [10,30,9,9]
+console.log(n1,n3,n5,n6) 
+// 10 9 undefined 22
 
 const [,[,nota]] = [[,8,8],[9,6,8]]
 console.log(nota)
