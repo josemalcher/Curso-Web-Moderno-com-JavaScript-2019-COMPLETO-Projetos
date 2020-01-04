ba97bc99539ad02128fab33b79466c735a1533a2
josemalcher <contato@josemalcher.net>
2020-01-03 22:05:36 -0300

    044 Operadores Aritmeticos

---
+++ 03-Javascript-Fundamentos/aritimetico.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a + b + c + d
+const sub  = d - c  
+const mult = a * b
+const div = d / a
+const modulo = a % 2
+
+console.log(soma, sub, mult, div, modulo) // 24 14 15 5 1
